772dead19e821bd8381f4b1afd84c4fb643a90e3
Om-paliwal <paliwalom2005@gmail.com>
2025-08-19 12:33:30 +0530

    Refactor countones function to improve logic and variable naming for clarity

--- Arrays/EASY/CountMaximumConsecutiveOne's.cpp
+++ Arrays/EASY/CountMaximumConsecutiveOne's.cpp
@@ -4,28 +4,26 @@ using namespace std;
 
 int countones(vector<int> & v1){
     int count = 0 ; 
-    int max = 0 ;
+    int maxCount = 0 ;
     for (int i = 0; i < v1.size(); i++)
     {
-        if((v1[i]==1 && v1[i+1]==1) || (v1[i]==1 && v1[i-1]==1) ){
+        if(v1[i]==1){
 
             count++ ; 
-            if(max<count){
-                max = count; 
-            }
+         maxCount = max(maxCount, count);
         }
         else{
             count = 0 ; 
         }
     }
 
-    return max ; 
+    return maxCount ; 
     
 }
 
 int main() 
 {
-    vector<int> v1 = { 1,0,1,1,1,0,0};
+    vector<int> v1 = { 1,0,1,1,1,1,0,1,1,1,1,1,1 ,0,0};
     int count = countones(v1); 
     cout << "The number of consecutive ones are  " << count ; 
 
